a395bfb7885206dfc42ba027b1e482a715856d11
Kaloian Ianev <kianev@gmail.com>
2017-12-22 11:33:38 +0200

    Mongoose used for connection to MongoDB atlas cloud

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,47 +1,95 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'handling GET requests /products'
-  })
+  Product.find().exec()
+    .then(docs => {
+      console.log(docs)
+        res.status(200).json(docs)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        error: error
+      })
+    });
 });
 
 router.post('/', (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
-  }
-  res.status(201).json({
-    message: 'handling POST requests /products',
-    createdProduct: product
-  })
+  });
+  product.save()
+    .then(result => {
+      console.log(result)
+      res.status(201).json({
+        message: 'handling POST requests /products',
+        createdProduct: product
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        error: error
+      })
+    });
 });
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  if(id === 'special'){
-    res.status(200).json({
-      message: 'You have discovered Special ID',
-      id: id
-    })
-  }else {
-    res.status(200).json({
-      message: 'You passed an ID'
-    })
-  }
+ Product.findById(id)
+   .exec()
+   .then(doc => {
+     console.log("from database", doc);
+     if(doc){
+       res.status(200).json(doc);
+     }else {
+       res.status(500).json({message: "No valid entry found for provided ID"});
+     }
+   })
+   .catch(err => {
+     console.log(err);
+     res.status(500).json({error: err});
+   });
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-      message: 'Updated Product!'
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
     })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted Product!'
-  })
+  const id = req.params.productId
+  Product.remove({_id: id}).exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+  "mongodb://node-shop:" + process.env.MONGO_ATLAS_PW +"@node-rest-shop-shard-00-00-nftmb.mongodb.net:27017,node-rest-shop-shard-00-01-nftmb.mongodb.net:27017,node-rest-shop-shard-00-02-nftmb.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin",
+  {
+    useMongoClient: true
+  }
+);
+
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
