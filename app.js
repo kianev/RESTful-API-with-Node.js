d76dfa7e4bbf6adc146d9756703d596585b7d1de
Kaloian Ianev <kianev@gmail.com>
2018-01-23 17:55:29 +0200

    User signup and delete routes and functionalities created, user email validation with regex and mongoose created

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 mongoose.connect(
   "mongodb://node-shop:" + process.env.MONGO_ATLAS_PW +"@node-rest-shop-shard-00-00-nftmb.mongodb.net:27017,node-rest-shop-shard-00-01-nftmb.mongodb.net:27017,node-rest-shop-shard-00-02-nftmb.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin",
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Not found.');
